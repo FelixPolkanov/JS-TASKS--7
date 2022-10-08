035ad9d0695cce41cef6ca9ad1432bd2c9b7c390
FelixPolkanov <felixpolkanov@gmail.com>
2022-10-08 09:37:00 +0300

    js-task-homework7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryItemsRef = document.querySelector(".gallery");
+const cardsMarkup = createCardsMarkup(galleryItems);
+galleryItemsRef.insertAdjacentHTML('beforeend', cardsMarkup);
 
-console.log(galleryItems);
+
+function createCardsMarkup(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class = "gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>
+</div>`;        
+}).join('');
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+
